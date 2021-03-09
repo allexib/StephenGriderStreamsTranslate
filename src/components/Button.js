55918a285e9f4a98f116d53d0f5550561ebc712a
allexib <ooomegasib@gmail.com>
2021-03-09 20:52:11 +0400

    12\14 405 Pulling From Multiple Context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class Button extends React.Component {
     renderSubmit(value) {
-    return  value === 'english' ? 'Submit' : 'Voorleggen';
-}
+        return value === 'english' ? 'Submit' : 'Voorleggen';
+    }
+
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {(value) => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
+        );
+    }
 
     render() {
         return (
-            <button className="ui button primary">
-            <LanguageContext.Consumer>
-                {(value) => this.renderSubmit(value)}
-            </LanguageContext.Consumer>
-            </button>
+            <ColorContext.Consumer>
+                {color => this.renderButton(color)}
+            </ColorContext.Consumer>
         );
     }
 }
